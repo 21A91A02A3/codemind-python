7bbaf0d13cb7c178b279e3803b120d726ca6d010
prasanth Tamiri <97613538+21A91A02A3@users.noreply.github.com>
2022-10-13 16:57:29 +0530

    n=int(input()) t=n s=n*n dc=0 while n>0:     n=n//10     dc +=1 x=s%10**dc if x==t:     print('Automorphic Number') else:     print('Not an Automorphic Number')

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,15 +1,12 @@
-import math
-def nod(a):
-    c=0
-    while a>0:
-        r=a%10
-        c+=1
-        a//=10
-    return c
-a=int(input())
-n=nod(a)
-asq=a*a
-if asq%math.pow(10,n)==a:
-    print("Automorphic Number")
+n=int(input())
+t=n
+s=n*n
+dc=0
+while n>0:
+    n=n//10
+    dc +=1
+x=s%10**dc
+if x==t:
+    print('Automorphic Number')
 else:
-    print("Not an Automorphic Number")
+    print('Not an Automorphic Number')
